d0aebfb4a81fc06d58fd1a159dfd89f860aea4e2
landon.mote <landon.mote@gmail.com>
2017-03-27 16:18:27 -0700

    Created pooled connections function

--- src/database/connection.rs
+++ src/database/connection.rs
@@ -1,12 +1,18 @@
 use std::thread;
+use std::env;
 
 use r2d2;
 use diesel::mysql::MysqlConnection;
 use r2d2_diesel::ConnectionManager;
+use dotenv::dotenv;
 
 pub fn database_connection() { 
+    dotenv().ok();
+
+    let url = env::var("DATABASE_URL").expect("DATABASE_URL must be set!");
+
     let config = r2d2::Config::default();
-    let manager = ConnectionManager::<MysqlConnection>::new("mysql://localhost:3306/");
+    let manager = ConnectionManager::<MysqlConnection>::new(url);
     let pool = r2d2::Pool::new(config, manager).expect("Failed to create pool");
 
     for _ in 0..10i32 {
